1d74306a19a6e4c57727bed662703f9ea1df3537
Dmitry <dmitryje@ya.ru>
2018-11-06 22:32:20 +0300

    update readme and add env

--- src/redux/create.js
+++ src/redux/create.js
@@ -1,19 +1,22 @@
 import ReduxThunk from 'redux-thunk';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { RootReducer } from './reducer';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import clientMiddleware from 'redux/middleware/clientMiddleware';
 
 export default function (initialState) {
+
   const middlewares = [
     ReduxThunk,
     clientMiddleware()
   ];
 
+  const composeEnhancer = process.env.REACT_APP_CONFIG === 'dev' ? composeWithDevTools : compose;
+
   return createStore(
     RootReducer,
     initialState,
-    composeWithDevTools(
+    composeEnhancer(
       applyMiddleware(...middlewares)
     )
   );
